6471a7f4283c04029c00ff006b78cac9d5daf4da
Dgakson <www.jenya96@gmail.com>
2024-10-28 18:32:01 +0300

    step1 (add child class)

--- main.py
+++ main.py
@@ -21,11 +21,17 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+        
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    pass
+
+best_student = Student('Roy', 'Djons', 'mail')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Mentor('Pavel', 'Mkrtchan')
 cool_mentor.courses_attached += ['Python']
  
 cool_mentor.rate_hw(best_student, 'Python', 10)
